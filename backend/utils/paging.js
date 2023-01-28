18f3199207ae90201b17ed6d1f29ff646d5fba90
arvind-owl <arvind082022@outlook.com>
2023-01-28 09:50:58 +0530

    pagination shorting

--- backend/utils/paging.js
+++ backend/utils/paging.js
@@ -1,3 +1,3 @@
 exports.paging = (arr, page = 1, pageSize = 15) => {
-  return arr.slice(pageSize * page - (pageSize * page) / page, pageSize * page);
+  return arr.slice(pageSize * page - pageSize, pageSize * page);
 };
